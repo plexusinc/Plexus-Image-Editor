4292657bc690b7096244cee2814b2c368a145faa
Tihan-Nico <tihannico@.github.com>
2020-07-12 19:42:55 +0200

    Initial commit

---
+++ app/src/main/java/com/plexus/photo_editor/OnSaveBitmap.java
@@ -0,0 +1,9 @@
+package com.plexus.photo_editor;
+
+import android.graphics.Bitmap;
+
+public interface OnSaveBitmap {
+    void onBitmapReady(Bitmap saveBitmap);
+
+    void onFailure(Exception e);
+}
