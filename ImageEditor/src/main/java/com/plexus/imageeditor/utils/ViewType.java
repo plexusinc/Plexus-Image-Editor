218032a70269e740a419cf491bcbf0293666fa4d
Tihan-Nico <tihannico@.github.com>
2020-07-12 20:06:40 +0200

    Initial Commit

---
+++ ImageEditor/src/main/java/com/plexus/imageeditor/utils/ViewType.java
@@ -0,0 +1,19 @@
+package com.plexus.imageeditor.utils;
+
+import com.plexus.imageeditor.views.PlexusImageEditorView;
+
+/**
+ * <p>
+ * Enum define for various operation happening on the {@link PlexusImageEditorView} while editing
+ * </p>
+ *
+ * @author <a href="https://github.com/burhanrashid52">Burhanuddin Rashid</a>
+ * @version 0.1.1
+ * @since 18/01/2017.
+ */
+public enum ViewType {
+    BRUSH_DRAWING,
+    TEXT,
+    IMAGE,
+    EMOJI
+}
--- app/src/main/java/com/plexus/photo_editor/OnSaveBitmap.java
+++
@@ -1,9 +0,0 @@
-package com.plexus.photo_editor;
-
-import android.graphics.Bitmap;
-
-public interface OnSaveBitmap {
-    void onBitmapReady(Bitmap saveBitmap);
-
-    void onFailure(Exception e);
-}
